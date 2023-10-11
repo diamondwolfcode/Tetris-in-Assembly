4f8b70fca98e338562961d19728c9aae55e793c6
diamongwolfcode <amitfilber1@gmail.com>
2023-10-11 16:15:46 +0300

    (kinda) made multipule pieces

--- AmitGame.asm
+++ AmitGame.asm
@@ -5,7 +5,7 @@ jumps
 p186
 DATASEG
 ;
-	gameName db 10,13,10,13, 'Tetris Plus - Press Q to Quit' ,10,13,10,13,'$'
+	gameName db 10,13,10,13, '	Tetris Plus - Press Q to Quit' ,10,13,10,13,'$'
 	score db 0
 	lines db 0
 	space db ' ','$'
@@ -35,24 +35,61 @@ DATASEG
 	db '==============' , 10, 13, '$'
 
 	screen_size dw 20
-	x_cord db 7 ;column 
-	y_cord db 6 ;row
-	x_cord2 dw 7,6,7,6
-	y_cord2 dw 7,6,7,6
+	x_cord1 db 7 ;column 
+	y_cord1 db 6 ;row
+	x_cord2 db 8
+	y_cord2 db 7
+	x_cord3 db 8 ;column 
+	y_cord3 db 6 ;row
+	x_cord4 db 6
+	y_cord4 db 7
 	color dw 0Eh ; color
 ;
 CODESEG
 
-proc setCursorPosition
+proc setCursorPosition1
 	pusha
-	mov dh, [y_cord] ; row
-	mov dl, [x_cord] ; column
+	mov dh, [y_cord1] ; row
+	mov dl, [x_cord1] ; column
 	mov bh, 0 ; page number
 	mov ah, 2
 	int 10h
 	popa
 	ret
-endp setCursorPosition
+endp setCursorPosition1
+
+proc setCursorPosition2
+	pusha
+	mov dh, [y_cord2] ; row
+	mov dl, [x_cord2] ; column
+	mov bh, 0 ; page number
+	mov ah, 2
+	int 10h
+	popa
+	ret
+endp setCursorPosition2
+
+proc setCursorPosition3
+	pusha
+	mov dh, [y_cord3] ; row
+	mov dl, [x_cord3] ; column
+	mov bh, 0 ; page number
+	mov ah, 2
+	int 10h
+	popa
+	ret
+endp setCursorPosition3
+
+proc setCursorPosition4
+	pusha
+	mov dh, [y_cord4] ; row
+	mov dl, [x_cord4] ; column
+	mov bh, 0 ; page number
+	mov ah, 2
+	int 10h
+	popa
+	ret
+endp setCursorPosition4
 
 proc drawBlack
 	pusha
@@ -83,19 +120,57 @@ endp drawChar
 proc down
 	pusha
 
-	call setCursorPosition
+	call setCursorPosition1
 
-	inc [y_cord]
+	inc [y_cord1]
 
-	cmp [y_cord], 17
+	cmp [y_cord1], 17
 	je pushUp
 
 	call drawBlack
 
-	call setCursorPosition
+	call setCursorPosition1
 
 	call drawChar
+; 
+	call setCursorPosition2
 
+	inc [y_cord2]
+
+	cmp [y_cord2], 17
+	je pushUp
+
+	call drawBlack
+
+	call setCursorPosition2
+
+	call drawChar
+; 
+	call setCursorPosition3
+
+	inc [y_cord3]
+
+	cmp [y_cord3], 17
+	je pushUp
+
+	call drawBlack
+
+	call setCursorPosition3
+
+	call drawChar
+; 
+	call setCursorPosition4
+
+	inc [y_cord4]
+
+	cmp [y_cord4], 17
+	je pushUp
+
+	call drawBlack
+
+	call setCursorPosition4
+
+	call drawChar
 	popa
 	ret
 endp down
@@ -103,17 +178,55 @@ endp down
 proc left
 	pusha
 
-	call setCursorPosition
+	call setCursorPosition1
 
+	dec [x_cord1]
 
-	dec [x_cord]
-
-	cmp [x_cord], 0
-	je pushRight
+	cmp [x_cord1], 17
+	je pushUp
 
 	call drawBlack
 
-	call setCursorPosition
+	call setCursorPosition1
+
+	call drawChar
+; 
+	call setCursorPosition2
+
+	dec [x_cord2]
+
+	cmp [x_cord2], 17
+	je pushUp
+
+	call drawBlack
+
+	call setCursorPosition2
+
+	call drawChar
+; 
+	call setCursorPosition3
+
+	dec [x_cord3]
+
+	cmp [x_cord3], 17
+	je pushUp
+
+	call drawBlack
+
+	call setCursorPosition3
+
+	call drawChar
+; 
+	call setCursorPosition4
+
+	dec [x_cord4]
+
+	cmp [x_cord4], 17
+	je pushUp
+
+	call drawBlack
+
+	call setCursorPosition4
 
 	call drawChar
 
@@ -124,19 +237,57 @@ endp left
 proc right
 	pusha
 
-	call setCursorPosition
+	call setCursorPosition1
 
-	inc [x_cord]
+	inc [x_cord1]
 
-	cmp [x_cord], 13
-	je pushRight
+	cmp [x_cord1], 17
+	je pushUp
 
 	call drawBlack
 
-	call setCursorPosition
+	call setCursorPosition1
 
 	call drawChar
+; 
+	call setCursorPosition2
 
+	inc [x_cord2]
+
+	cmp [x_cord2], 17
+	je pushUp
+
+	call drawBlack
+
+	call setCursorPosition2
+
+	call drawChar
+; 
+	call setCursorPosition3
+
+	inc [x_cord3]
+
+	cmp [x_cord3], 17
+	je pushUp
+
+	call drawBlack
+
+	call setCursorPosition3
+
+	call drawChar
+; 
+	call setCursorPosition4
+
+	inc [x_cord4]
+
+	cmp [x_cord4], 17
+	je pushUp
+
+	call drawBlack
+
+	call setCursorPosition4
+
+	call drawChar
 	popa
 	ret
 endp right
@@ -157,10 +308,6 @@ start:
 	int 21h	
 
 	mov dx, offset openingscreen
-	mov ah, 9h
-	int 21h
-
-	mov dx, offset lines
 	mov ah, 9h
 	int 21h
 
@@ -185,17 +332,17 @@ printRight:
 jmp getKey
 
 pushRight:
-	mov [x_cord], 12
+	mov [x_cord1], 12
 
 jmp getKey
 
 pushLeft:
-	dec [x_cord]
+	dec [x_cord1]
 
 jmp getKey
 
 pushUp:
-	dec [y_cord]
+	dec [y_cord1]
 
 jmp getKey
 
